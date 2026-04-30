b19d02da82887fa097f147d1e6a86c9ceb3dffd0
mteregh <mtguerra@miuandes.cl>
2026-04-30 16:34:06 -0400

    Expand routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "owners#index"
-
-  resources :owners, only: [:index, :show]
-  resources :pets, only: [:index, :show]
-  resources :vets, only: [:index, :show]
-  resources :appointments, only: [:index, :show]
+  resources :owners
+  resources :pets
+  resources :vets
+  resources :appointments do
+    resources :treatments, except: [:index, :show]
+  end
   
 end

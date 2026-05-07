7eaab9a10aeba6619a54c7091d879df81ff7eeca
mteregh <mtguerra@miuandes.cl>
2026-05-07 19:11:04 -0400

    See photo

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -45,7 +45,7 @@ class PetsController < ApplicationController
   end
 
   def pet_params
-    params.require(:pet).permit(:name, :species, :breed, :date_of_birth, :weight, :owner_id)
+    params.require(:pet).permit(:name, :species, :breed, :date_of_birth, :weight, :owner_id, :photo)
   end
   
 end
